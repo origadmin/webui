8e78414e15caaf6785787687cb61b8dacf968c37
godcong <jumbycc@163.com>
2024-09-23 23:31:29 +0800

    feat(frontend): add base

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import { ThemeProvider } from "@/components/theme-provider";
-import "./styles/globals.css";
+// import "./styles/globals.css";
 import "./assets/index.css"
 
 createRoot(document.getElementById("root")!).render(
