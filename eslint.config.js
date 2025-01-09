ac488e8de08b3b9d3b155e571acb760b8559f085
godcong <jumbycc@163.com>
2025-01-10 00:55:37 +0800

    build(lint): replace biome with eslint for linting
    - Remove biome configuration and related files
    - Add eslint, prettier configurations and related files
    - Update package.json scripts for linting and formatting
    - Modify eslint configuration to use recommended rules and plugins

--- eslint.config.js
+++ eslint.config.js
@@ -1,15 +1,30 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
-import pluginReact from "eslint-plugin-react";
+import {fixupConfigRules} from '@eslint/compat';
+import js from '@eslint/js';
+import reactHooks from 'eslint-plugin-react-hooks';
+import pluginReact from "eslint-plugin-react"
+import globals from 'globals';
+import ts from 'typescript-eslint';
 
-
-/** @type {import('eslint').Linter.Config[]} */
 export default [
-    {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
-    {files: ["**/*.js"], languageOptions: {sourceType: "script"}},
-    {languageOptions: {globals: {...globals.browser, ...globals.node}}},
-    pluginJs.configs.recommended,
-    ...tseslint.configs.recommended,
-    pluginReact.configs.flat.recommended,
+    {languageOptions: {globals: globals.browser}},
+    js.configs.recommended,
+    ...ts.configs.recommended,
+    ...fixupConfigRules([
+        {
+            ...pluginReact.configs.recommended,
+            settings: {
+                react: {version: 'detect'},
+            },
+        },
+        pluginReact.configs["jsx-runtime"],
+    ]),
+    {
+        plugins: {
+            'react-hooks': reactHooks,
+        },
+        rules: {
+            ...reactHooks.configs.recommended.rules,
+        },
+    },
+    {ignores: ['dist/', 'node_modules/']},
 ];
