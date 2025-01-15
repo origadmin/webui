afc1602af6408b7b999a0e073a5ec217c6152afe
godcong <jumbycc@163.com>
2025-01-16 01:43:57 +0800

    refactor(components): adjust footer layout and responsiveness
    - Remove unnecessary padding and added gap property to footer
    - Adjust inner div to use justify-end instead of justify-center
    - Make links list full width on smaller screens for better distribution

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,18 +1,20 @@
 import React from 'react';
 
+type LinkProps = {
+  key?: string;
+  title: React.ReactNode;
+  href?: string;
+  blank?: boolean;
+};
+
 export type FooterProps = {
-  links?: {
-    key?: string;
-    title: React.ReactNode;
-    href?: string;
-    blank?: boolean;
-  }[];
+  links?: LinkProps[];
   copyright?: string;
 };
 
 export function Footer(props?: FooterProps) {
-  const { links, copyright } = props && {
-    links: [
+  const {
+    links = [
       {
         title: 'FAQs',
       },
@@ -26,21 +28,20 @@ export function Footer(props?: FooterProps) {
         title: 'Refund Policy',
       },
     ],
-    copyright: 'OrigAdmin',
-  };
-  console.log(links, 'copyright:', copyright);
+    copyright = 'OrigAdmin',
+  } = props || {};
 
   return (
     <footer className='border-grid border-t flex flex-col gap-2 p-2 items-center justify-center md:px-8 md:py-2'>
       <div className='flex w-full flex-col items-center justify-end gap-2 p-2 xl:flex-row'>
         <div>
           <ul className='flex w-full flex-wrap items-center justify-center gap-3 sm:flex-nowrap md:gap-10'>
-            {links?.flatMap((link: { key?: string; title: React.ReactNode; href?: string; blank?: boolean }) => (
+            {links?.flatMap((link: LinkProps) => (
               <li>
                 <a
+                  className='text-sm font-medium text-zinc-500 hover:text-zinc-950 dark:text-zinc-400'
                   target={link.blank ? 'blank' : undefined}
                   href={link.href || '#'}
-                  className='text-sm font-medium text-zinc-500 hover:text-zinc-950 dark:text-zinc-400'
                 >
                   {link.title}
                 </a>
